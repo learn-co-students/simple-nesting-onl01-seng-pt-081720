502a528f8d155bcceb40616793f06a61a611dcc6
Elizabeth Cobb <cobbeec@gmail.com>
2020-08-28 23:47:11 +0000

    Done.

--- nested.rb
+++ nested.rb
@@ -61,12 +61,6 @@ end
 
 
 def adding_matz
- programmer_hash[:yukihiro_matsumoto] = {
-    known_for: "Ruby",
-    languages:
-      ["LISP", "C"]
-    }
-
 	programmer_hash = 
  		{
         :grace_hopper => {
@@ -82,8 +76,13 @@ def adding_matz
           :languages => ["C"]
         }
      }
-
+ programmer_hash[:yukihiro_matsumoto] = {
+    known_for: "Ruby",
+    languages:
+      ["LISP", "C"]
+    }
     
+ return programmer_hash
 end
 
 def changing_alan
@@ -104,6 +103,8 @@ def changing_alan
      }
      alans_new_info = "GUI"
      
+  programmer_hash[:alan_kay][:known_for] = "GUI"
+  programmer_hash  
      
 end
 
@@ -123,7 +124,7 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-  programmer_hash[:dennis_ritchie][:languages] << "Assembly"
+    programmer_hash[:dennis_ritchie][:languages] << "Assembly"
   programmer_hash
 
 end
