79915e4fbbfa5ec7bcd192bc46454e82907ee78a
Myles Janik <mylesjanik@gmail.com>
2020-08-31 04:28:54 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -85,8 +85,10 @@ def adding_matz
         }
      }
 
-    programmer_hash[:yukihiro_matsumoto] = [:known_for] = "Ruby"
-    programmer_hash[:yukihiro_matsumoto] = [:languages] = ["LISP", "C"]
+    programmer_hash[:yukihiro_matsumoto] = [:known_for]
+    programmer_hash[:known_for] = "Ruby"
+    programmer_hash[:yukihiro_matsumoto] = [:languages]
+    programmer_hash[:languages] = ["LISP", "C"]
 end
 
 def changing_alan
